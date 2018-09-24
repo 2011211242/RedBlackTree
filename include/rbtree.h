273684e3c6dd3fa227a881a8f448fa3a579e26f5
2011211242 <2990936748@qq.com>
2018-09-23 17:52:28 -0700

    add rotate, element add function

--- include/rbtree.h
+++ include/rbtree.h
@@ -1,44 +1,38 @@
 #ifndef _RBTREE_
 #define _RBTREE_
-
 #include <iostream>
 #include <stack>
 
 namespace rdtree {
-
 using namespace std;
 
 
 enum color {
-    black = 0,
-    red = 1,
+    BLACK = 0,
+    RED = 1,
 };
 
 
 template <typename T>
 class node {
 public:
-    node(){ l = nullptr, r = nullptr, cl = black; }
-    node(T a): val(a) { l = nullptr, r = nullptr, cl = black; }
+    node(){ l = nullptr, r = nullptr, cl = BLACK; }
+    node(const T& a): val(a) { l = nullptr, r = nullptr, cl = BLACK; }
 
     node<T>* left() { return l; }
     node<T>* right() { return r; }
+    node<T>* parent() { return p; }
 
     void set_left(node<T>* ptr) { l = ptr; }
     void set_right(node<T>* ptr) { r = ptr; }
-
-    ~node() { 
-        if ( l ) l -> ~node(); 
-        if ( r ) r -> ~node(); 
-        delete l;
-        delete r;
-        l = r = nullptr;
-        val.~T();
-    }
-
+    void set_parent(node<T>* ptr) { p = ptr; }
+    void set_color(const color c) { cl = c; }
+    void set_value(const T& val) { this -> val = val; }
+    T value(){ return val; }
 private:
     node<T>* l; 
     node<T>* r;
+    node<T>* p;
     color cl;
     T val;
 };
@@ -47,18 +41,146 @@ private:
 template <typename T>
 class rdtree {
 public:
-    rdtree() { root = nullptr; }
+    rdtree() { 
+        nil = new node<T>; 
+        nil -> set_left(nil);
+        nil -> set_right(nil);
+        nil -> set_parent(nil);
+        root = nil;
+    }
+
     ~rdtree();
+    void add(const T &val);
+
 private:
+    void insert_fixup(node<T>* const x);
+    void left_rotate(node<T>* const x);
+    void right_rotate(node<T>* const x);
+
     node<T> * root;
+    node<T> * nil;
 };
 
 
-template <class T>
-rdtree<T>::~rdtree(){
-    if ( root ) root -> ~node();
+template <typename T>
+rdtree<T>::~rdtree() {
+    stack<node<T> *> stk;
+    if (root != nil) stk.push(root);
+
+    while(!stk.empty()) {
+        node <T> * ptr = stk.top();
+        stk.pop();
+        while ( ptr ) {
+            node <T> * left = ptr -> left();
+            node <T> * right = ptr -> right();
+
+            if (right != nil)
+                stk.push(right);
+
+            ptr -> set_left(nullptr);
+            ptr -> set_right(nullptr);
+            delete ptr;
+            ptr = left;
+        }
+    }
+
+    delete nil;
+    nil = nullptr;
     root = nullptr;
 }
+
+
+template <typename T>
+void rdtree<T>::add(const T &val) {
+    node<T>* parent = nil;
+    node<T>* ptr = root;
+
+    while( ptr != nil ) {
+        parent = ptr;
+        if ( val < ptr -> val) ptr = ptr -> left();
+        else ptr = ptr -> right();
+    }
+
+    node<T>* z = new node<T>( val );
+
+    if (ptr == nil) 
+        root = z;
+    else {
+        if ( val < parent -> value() ) parent -> set_left(z);
+        else parent -> set_right(z);
+    }
+
+    z -> set_parent( parent );
+    z -> set_left(nil);
+    z -> set_right(nil);
+    z -> set_color(RED);
+
+    insert_fixup();
+
+}
+
+
+template <typename T>
+void rdtree<T>::insert_fixup(node<T>* const x) {
+    //if ()
+}
+
+
+template <typename T>
+void rdtree<T>::left_rotate(node<T>* const x) {
+    node<T> * const right = x -> right();
+
+    if( x == root ) {
+        root = right;
+    }
+    else {
+        if (x == x -> parent() -> left()) {
+            x -> parent() -> set_left(right);
+        }
+        else {
+            x -> parent() -> set_right(right);
+        }
+    }
+    right -> set_parent(x -> parent());
+
+
+    x -> set_parent(right);
+    x -> set_right(right -> left());
+
+    if (right -> left() != nil) {
+        right -> left() -> set_parent(x);
+    }
+
+    right -> set_left(x);
+}
+
+
+template <typename T>
+void rdtree<T>::right_rotate(node<T>* const x) {
+    node<T> * const left = x -> left();
+    if (x == root) {
+        root = left;
+    }
+    else {
+        if (x == x -> parent() -> left()) {
+            x -> parent() -> set_left(left);
+        }
+        else {
+            x -> parent() -> set_right(left);
+        }
+    }
+    left -> set_parent(x -> parent);
+
+    x -> set_parent(left);
+    x -> set_left(left -> right());
+
+    if (left -> right() != nil) {
+        left -> right() -> set_parent(x);
+    }
+    left -> set_right(x);
+}
+
+
 
 
 };
--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,16 @@
 #include <rbtree.h>
+#include <vector>
+#include <iostream>
+using namespace std;
 
 int main() {
     rdtree::node<int> a;
     rdtree::node<int> b(1);
+    rdtree::rdtree<int> tree;
+
+    
+    vector<int> vec;
+    vec.~vector<int>();
+
+    cout << vec.size() << endl;
 }
